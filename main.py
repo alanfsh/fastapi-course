4fae674b53ecf7c5cc8f2a9bfe2a1362861129f7
alanfsh <zero_ikniz@hotmail.com>
2022-11-09 23:18:23 -0600

    cookies_and_headers

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pydantic import EmailStr
 #FastAPI
 from fastapi import FastAPI 
 from fastapi import status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 # inicializando app como un objeto de FastAPI
 app = FastAPI()
@@ -220,3 +220,29 @@ def update_person(
 )
 def login(username: str = Form(...), password: str = Form(...)):
     return LoginOut(username=username)
+
+# COOKIES AND HEADERS PARAMETERS
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
